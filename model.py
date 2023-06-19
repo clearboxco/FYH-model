cefb747cc67897b2f63041929462e04f0eb4c496
ryansherby <ryan.m.sherby@gmail.com>
2023-06-18 23:59:29 -0500

    outline

---
+++ model.py
@@ -0,0 +1,15 @@
+import json
+
+# PREFIX: STREAM IN DATA
+
+## Used to be from S3 bucket, now will allow for stream from SQL database
+
+# PART 1: READ IN JSON DATA
+
+# PART 2: CONFIGURE MODEL SETTINGS
+
+# PART 3: PREPROCESS STREAMED DATA
+
+# PART 4: RUN MODEL
+
+# PART 5: POST MODEL DATA
