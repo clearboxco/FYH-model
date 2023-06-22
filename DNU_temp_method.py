5caad06574ee20892461adfd37606af516cd9578
ryansherby <ryan.m.sherby@gmail.com>
2023-06-21 19:47:54 -0500

    com

---
+++ DNU_temp_method.py
@@ -0,0 +1,90 @@
+# TEMPORARY STREAM IN DATA TO GENERATE LOCAL CSV
+
+# WILL BE DEPRECATED IN FINAL MODEL
+
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+aws_bucket=os.getenv('AWS_RE_BUCKET')
+aws_key=os.getenv('AWS_RE_BUCKET_KEY')
+aws_secret=os.getenv('AWS_RE_BUCKET_SECRET')
+
+date=str(sys.argv[1])
+state="CA"
+states=['AL',
+ 'NE',
+ 'AK',
+ 'NV',
+ 'AZ',
+ 'NH',
+ 'AR',
+ 'NJ',
+ 'CA',
+ 'NM',
+ 'CO',
+ 'NY',
+ 'CT',
+ 'NC',
+ 'DE',
+ 'ND',
+ 'DC',
+ 'OH',
+ 'FL',
+ 'OK',
+ 'GA',
+ 'OR',
+ 'HI',
+ 'PA',
+ 'ID',
+ 'PR',
+ 'IL',
+ 'RI',
+ 'IN',
+ 'SC',
+ 'IA',
+ 'SD',
+ 'KS',
+ 'TN',
+ 'KY',
+ 'TX',
+ 'LA',
+ 'UT',
+ 'ME',
+ 'VT',
+ 'MD',
+ 'VA',
+ 'MA',
+ 'VI',
+ 'MI',
+ 'WA',
+ 'MN',
+ 'WV',
+ 'MS',
+ 'WI',
+ 'MO',
+ 'WY',
+ 'MT',
+]
+
+df_list=[]
+for st in states:
+    try:
+        df = pd.read_parquet(path=aws_bucket+f"/{date}/{st}.parquet",storage_options={"key":aws_key,"secret":aws_secret})
+        df_list.append(df)
+    except:
+        pass
+    
+df=pd.concat(df_list)
+
+df=df.applymap(lambda x: np.nan if x=='nan' else x)
+
+prop_types=['Single Family Residential', 'Mobile/Manufactured Home','Townhouse', 'Multi-Family (2-4 Unit)', 'Condo/Co-op','Multi-Family (5+ Unit)']
+
+df=df[df['PROPERTY TYPE'].isin(prop_types)]
+
+df=df.dropna(subset=['ADDRESS','CITY','STATE OR PROVINCE','ZIP OR POSTAL CODE','PROPERTY TYPE','PRICE','BEDS','BATHS','SQUARE FEET','YEAR BUILT','URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)'],axis=0).reset_index(drop=True).apply(lambda row: pd.to_numeric(row,errors='ignore'))
+
+df.to_csv(path_or_buf=f"./{date}_temp_data.csv")
--- model.py
+++ model.py
@@ -1,15 +1,143 @@
 import json
+import os
 
-# PREFIX: STREAM IN DATA
+import pandas as pd
+import numpy as np
+import random
+
+from flask import Flask
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import NearestNeighbors
+
+from scipy.spatial.distance import euclidean
+
+df=pd.read_csv("2023-06-16_temp_data.csv")
+
+# METHOD ABOVE WILL BE DEPRECATED
 
 ## Used to be from S3 bucket, now will allow for stream from SQL database
 
+## Need to run check here if SQL pull returns no data and push to Message
+
+# Prefix: DEFINE APP AND ENDPOINT
+
+app = Flask(__name__)
+
+@app.route('/post', methods=['POST'])
+def post_endpoint():
+    input_data = request.get_json()
+
 # PART 1: READ IN JSON DATA
+
+def parse_json(input_data):
+    submission_type=input_data['submission_type']
+    
+    
+    filters=input_data['filters']
+
+    price_max=filters['price']['max']
+    price_min=filters['price']['min']
+    
+    property_type=filters["property_type"]
+    
+    state=filters['location']['state']
+    city=filters['location']['city']
+    zip=filters['location']['zip']
+    
+    year_built_max=filters['year_built']['max']
+    year_built_min=filters['year_built']['min']
+
+
+    data=input_data['data']
+    
+    bedrooms=data['bedrooms']
+    bathrooms=data['bathrooms']
+    sqft=data['sqft']
+
 
 # PART 2: CONFIGURE MODEL SETTINGS
 
+def configure_model_weights(bds,bas,sqft):
+    bd_weight=1
+    ba_weight=1
+    sqft_weight=1
+    
+    if submission_type==2:
+        if bds==0:
+            bd_weight=0
+        if bas==0:
+            ba_weight=0
+        if sqft==0:
+            sqft_weight=0
+
 # PART 3: PREPROCESS STREAMED DATA
+
+def shuffle_data(df:pd.DataFrame):
+    random_state=random.randint(0,999)
+    
+    df.sample(frac=1, replace=False, random_state=random_state)
+    
+    return random_state
+
+def preprocess_data():
+    NN_df=df[['BEDS','BATHS','SQUARE FEET']]
+    input_df=pd.Dataframe({'BEDS':[bedrooms],'BATHS':[bathrooms],'SQUARE FEET':[sqft]})
+    
+    # Define the transformations for each column
+    preprocessor = ColumnTransformer(
+    transformers=[
+        ('minmax_scaler',MinMaxScaler(),['BEDS','BATHS','SQUARE FEET'])
+    ])
+
+    # Apply the transformations in a pipeline
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor)])
+
+    NN_np=pipeline.fit_transform(NN_df)
+    input_np=pipeline.transform(input_df)
+    
+    
+    return NN_np
+
 
 # PART 4: RUN MODEL
 
+def weighted_euclidian(x,y):
+    weights=np.array(bd_weight,ba_weight,sqft_weight)
+    
+    return euclidean
+
+def create_model(input,data:np.array):
+    knn=NearestNeighbors(n_neighbors=data.shape[0],metric=weighted_euclidian)
+    knn.fit(data)
+    
+    distances, indices = knn.kneighbors(input)
+    
+    return distances, indices
+
+
+def get_top(indices) -> pd.DataFrame:
+    df_list=[]
+    
+    c=0
+    for index in indices[0]:
+        if c>6:
+            break
+        
+        df_list.append(df.loc[index])
+        
+        c+=1
+        
+    return pd.concat(df_list)
+        
+        
+        
+    
+
+
 # PART 5: POST MODEL DATA
+
+def prepare_json():
+    
